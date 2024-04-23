8863be73a651ae81c3a05fc3278c366e4e59373a
MOHAMMAD HASSAN <159164628+mhassanworks@users.noreply.github.com>
2024-04-23 19:51:45 +0530

    Create Implementation of structure using pointers.cpp

---
+++ Implementation of structure using pointers.cpp
@@ -0,0 +1,18 @@
+// C++ program to demonstrate Pointer to Structure
+#include <iostream>
+
+using namespace std;
+
+struct point {
+	int value;
+};
+
+int main()
+{
+
+	struct point g;
+	// Initialization of the structure pointer
+	struct point* ptr = &g;
+
+	return 0;
+}
